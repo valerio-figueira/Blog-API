45ce180a7e142a11bba86736de1f9a7d944a8c5d
valerio-figueira <j.valerio.figueira@gmail.com>
2022-12-09 21:27:00 -0300

    updating new files to deploy on netlify

---
+++ functions/api.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const serverless = require('serverless-http')
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({
+    'path': 'Home',
+    'message': 'Hello, world!'
+  })
+})
+
+router.get('/json', (req, res) => {
+    res.json({
+      'path': 'json',
+      'author': 'Valerio Figueira'
+    })
+})
+
+app.use('/.netlify/functions/api', router)
+
+module.exports.handler = serverless(app)
