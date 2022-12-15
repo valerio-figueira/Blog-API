fa2d2506068e41927aa05e831df1844f2bf6d52a
valerio-figueira <j.valerio.figueira@gmail.com>
2022-12-15 19:02:17 -0300

    new update

--- functions/api.js
+++ functions/api.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 
     // MIDDLEWARE FOR STATIC FILES
     app.use(express.static(path.join(__dirname, 'public')));
-    app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')))
+    app.use(favicon(path.join('../My API/public/', 'favicon.ico')))
 
     // JSON CONFIG IN MIDDLEWARES
     app.use(
